23cd3d2564ad39c7d62e6c796c4e0d3c8db319e3
Sasmita <sasmitapradhan01@gmail.com>
2022-11-18 16:38:11 -0500

    Value first commit.

---
+++ src/main/java/dan/danvega/greetingservicevalue/GreetingServiceValueApplication.java
@@ -0,0 +1,13 @@
+package dan.danvega.greetingservicevalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingServiceValueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingServiceValueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dan/danvega/greetingservicevalue/GreetingServiceValueApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.danvega.greetingservicevalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingServiceValueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
